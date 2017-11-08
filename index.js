655762f387529e8d41edf732f63c655d09f57a44
Maverick Tesini <mavericktesini@gmail.com>
2017-11-08 19:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 let newID = oldID + 1000000000;
-Boolean ageIsValid = Number.isInteger(currentAge);
+Boolean ageIsValid; 
+Number.isInteger(currentAge);
