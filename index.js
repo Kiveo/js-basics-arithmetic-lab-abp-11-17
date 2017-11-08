2b6c8d027436691f6ade1fc6109a5ec91e1c575d
Maverick Tesini <mavericktesini@gmail.com>
2017-11-08 19:47:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ var ageIsValid = false;
 ageIsValid = Number.isInteger(currentAge);
 var randomNumber = Math.random(1000000001, 1000000019);
 var randomInteger = Math.floor(randomNumber);
-var randomUserID = Number.round(randomInteger, 10);
+var randomUserID = Number.random(randomInteger, 10);
